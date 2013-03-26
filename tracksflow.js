f6ad24761bdabb832f412937cae8fad566c21bf8
funua <funsersver@gmail.com>
2013-03-26 23:11:53 +0200

    first commit

---
+++ tracksflow.js
@@ -0,0 +1,19 @@
+var arr=$.makeArray($('.pile #clickArea'));
+var delay = 5000;
+function func(){
+	if (arr.length == 0) {
+		alert('All done');
+		return false;
+	}
+	var item = arr.shift();
+	$(item).click();
+	setTimeout(function(){
+		var a = document.createElement('a');
+		var name = $('.track .artist').html()+' - '+$('.track .title').html();
+		a.href = $('#audioplayer').attr('src');
+		a.download = name;
+		a.click();
+		func();
+	}, delay)
+}
+func();
